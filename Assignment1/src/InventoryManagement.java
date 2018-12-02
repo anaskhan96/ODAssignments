3f693aa0e1fed3d1b30399be90f8bc16306d0eae
Anas Khan <anas.khan96@outlook.com>
2018-12-02 13:57:41 +0530

    added code for assignment1

---
+++ Assignment1/src/InventoryManagement.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class InventoryManagement {
+
+    /* Parse given command line arguments and place them in the map for future reference */
+    private static void parseCmdArgs(String[] arguments, List<String> flagsList, Map<String, String> argumentsMap) throws Exception {
+        for (int i = 0; i < arguments.length; i += 2) {
+            String flag = arguments[i].substring(1);
+            if (flagsList.contains(flag)) {
+                if (!(flag.equals("name") || argumentsMap.containsKey("name")))
+                    throw new Exception("Flag name should be provided first");
+                argumentsMap.put(flag, arguments[i + 1]);
+            } else
+                throw new IllegalArgumentException("Accepted arguments: -name, -price, -quantity, -type");
+        }
+        if (!argumentsMap.containsKey("type")) throw new MissingFormatArgumentException("Argument type is mandatory");
+    }
+
+    /* Add more items as per the user's wish */
+    private static void addMoreItems(List<Item> itemList) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("\nDo you want to enter details of any other item? (y/n): ");
+        // loop till the user wants to add more items
+        while (reader.next().charAt(0) == 'y') {
+            System.out.print("Name: ");
+            String name = reader.next();
+            System.out.print("Price: ");
+            double price = reader.nextDouble();
+            System.out.print("Quantity: ");
+            int quantity = reader.nextInt();
+            System.out.print("Type (raw, manufactured, or imported): ");
+            String type = reader.next();
+            try {
+                itemList.add(new Item(name, price, quantity, type));
+                System.out.printf("Item %s added!\n", name);
+            } catch (IllegalArgumentException iae) {
+                // type provided was not an accepted input
+                System.out.println("Argument type should either be raw, manufactured, or imported");
+                System.out.println("Item discarded!\n");
+            }
+            System.out.print("\nDo you want to enter details of any other item? (y/n): ");
+        }
+    }
+
+    /* Display info about each item from the item list */
+    private static void displayAllItems(List<Item> itemList) {
+        for (Item item : itemList) {
+            System.out.println("----------");
+            item.display();
+        }
+        System.out.println("----------");
+    }
+
+    public static void main(String[] args) {
+        final List<String> flags = new ArrayList<>();
+        flags.add("name");
+        flags.add("price");
+        flags.add("quantity");
+        flags.add("type");
+
+        Map<String, String> argumentsMap = new HashMap<>();
+        try {
+            parseCmdArgs(args, flags, argumentsMap);
+        } catch (Exception e) {
+            System.out.println(e.getClass() + ": " + e.getMessage());
+            System.exit(1);
+        }
+
+        List<Item> items = new ArrayList<>();
+        try {
+            items.add(new Item(argumentsMap.getOrDefault("name", "-"), Double.parseDouble(argumentsMap.getOrDefault("price", "10")), Integer.parseInt(argumentsMap.getOrDefault("quantity", "1")), argumentsMap.get("type")));
+        } catch (IllegalArgumentException iae) {
+            // type provided was not an accepted input
+            System.out.println("Argument type should either be raw, manufactured, or imported");
+            System.exit(1);
+        }
+        System.out.print("Item added!\n");
+
+        addMoreItems(items);
+        displayAllItems(items);
+    }
+}
---
+++ Assignment1/src/Item.java
@@ -0,0 +1,51 @@
+class Item {
+    enum ItemType {
+        raw, manufactured, imported
+    }
+
+    private String name;
+    private double price;
+    private int quantity;
+    private ItemType type;
+
+    Item(String name, double price, int quantity, String type) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        // will throw an exception if type is not raw, manufactured, or imported
+        this.type = ItemType.valueOf(type);
+    }
+
+    /* Calculates tax on the final price summed up by quantities of the item */
+    double calculateTax() {
+        double salesTax;
+        double totalPrice = this.price * this.quantity;
+        switch (this.type) {
+            case raw:
+                salesTax = 0.125 * totalPrice;
+                break;
+            case manufactured:
+                salesTax = 0.125 * totalPrice + 0.2 * (totalPrice + 0.125 * totalPrice);
+                break;
+            default:
+                salesTax = 0.1 * totalPrice;
+                double finalPrice = salesTax + totalPrice;
+                if (finalPrice <= 100) salesTax += 5;
+                else if (finalPrice <= 200) salesTax += 10;
+                else salesTax += 0.05 * finalPrice;
+        }
+        return salesTax;
+    }
+
+    /* Displays info about the item, along with the tax and final amount */
+    void display() {
+        double salesTax = calculateTax();
+        System.out.printf("Item name: %s\n", this.name);
+        System.out.printf("Item price: %.2f\n", this.price);
+        System.out.printf("Item quantity: %d\n", this.quantity);
+        double finalPrice = this.price * this.quantity;
+        System.out.printf("Final price: %.2f\n", finalPrice);
+        System.out.printf("Tax applied: %.2f\n", salesTax);
+        System.out.printf("Total amount: %.2f\n", (salesTax + finalPrice));
+    }
+}
