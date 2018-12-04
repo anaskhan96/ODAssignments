a633f33141c72e1db2f19e25f62a453299190091
Anas Khan <anas.khan96@outlook.com>
2018-12-05 03:06:43 +0530

    test changes

--- Assignment1/tests/ItemTest.java
+++ Assignment1/tests/ItemTest.java
@@ -1,15 +1,19 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ItemTest {
 
-    @org.junit.jupiter.api.Test
-    void calculateTax() {
+    @Test
+    @DisplayName("should calculate sales tax correctly according to different item types")
+    void calculateTaxTest() {
         Item testItem1 = new Item("testItem1", 50.67, 5, "raw");
         Item testItem2 = new Item("testItem2", 89.32, 8, "manufactured");
         Item testItem3 = new Item("testItem3", 34.81, 4, "imported");
 
-        assertEquals(Math.round(testItem1.calculateTax() * 100) / 100.0, 31.67);
-        assertEquals(Math.round(testItem2.calculateTax() * 100) / 100.0, 250.10);
-        assertEquals(Math.round(testItem3.calculateTax() * 100) / 100.0, 23.92);
+        assertAll(() -> assertEquals(Math.round(testItem1.calculateTax() * 100) / 100.0, 31.67),
+                () -> assertEquals(Math.round(testItem2.calculateTax() * 100) / 100.0, 250.10),
+                () -> assertEquals(Math.round(testItem3.calculateTax() * 100) / 100.0, 23.92));
     }
 }
