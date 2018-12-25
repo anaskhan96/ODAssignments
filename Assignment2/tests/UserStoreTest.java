d8f1465ee4e497df1f45d3eea4feb608217b0a8f
Anas Khan <anas.khan96@outlook.com>
2018-12-26 01:35:38 +0530

    boilerplate test draft

---
+++ Assignment2/tests/UserStoreTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserStoreTest {
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+
+    @Test
+    void saveAndRestore() {
+    }
+}
